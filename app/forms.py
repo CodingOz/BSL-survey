aad79c3bd813539e0ae7fa13f4a36b6453871234
CodingOz <sc23os@leeds.ac.uk>
2025-12-21 18:31:20 +0000

    fixed data consent

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,19 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, BooleanField
+from wtforms.validators import DataRequired
 from wtforms import FileField, SubmitField
 from flask_wtf.file import FileAllowed
 
 def make_video_upload_form(letters):
     class VideoUploadForm(FlaskForm):
+        consent_video_deletion = BooleanField(
+            'I understand that my videos will be used only for key-point extraction and that the original video will be permanently deleted afterwards.',
+            validators=[DataRequired(message="You must agree to this consent statement.")]
+        )
+        
+        consent_data_usage = BooleanField(
+            'I consent to the key-point tracking data extracted from my video being used to train and evaluate machine-learning models.',
+            validators=[DataRequired(message="You must agree to this consent statement.")]
+        )
         submit = SubmitField("Submit")
 
     for letter in letters:
