4a7197e9277621d9d85c4b6d56a3ca4758d7797f
CodingOz <sc23os@leeds.ac.uk>
2025-12-21 17:50:48 +0000

    fixed data limit to 500mb

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'wdefrgthnmjghfytdreasdvbgmuyjthrgdxfhmuyjrhtgfyou-will-never-guess-this-key-1234-5678-91011-1213-1415'
 
-MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100MB
+MAX_CONTENT_LENGTH = 500 * 1024 * 1024  # 1gb max upload size
